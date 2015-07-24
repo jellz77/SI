a3d25ca5ae6cf198d5ff6e8deda4eba119f726c0
jellz77 <jschuman77@gmail.com>
2015-07-23 20:30:37 -0400

    Update mpcontroller.php

--- controllers/mpcontroller.php
+++ controllers/mpcontroller.php
@@ -6,7 +6,7 @@ class MpController{
 	public function view(){
 		$clnt=isset($_GET['clnt']) ? $_GET['clnt'] : '';
 		$mttr=isset($_GET['mttr']) ? $_GET['mttr'] : '';
-		require_once('models/mpmodel.php');		
+		require_once('models/mpmodel.php');asdf		
 		$mp=MpModel::show($clnt,$mttr);		
 		$mp1=MpModel::coddsc();
 		require_once('views/pages/mp.php');	

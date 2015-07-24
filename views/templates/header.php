fd039b5cf29e85faf610d465bbaa4df9727f4cf9
jellz77 <jschuman77@gmail.com>
2015-07-24 12:53:45 -0400

    Changes to navbar
    Updated navbar css and removed divs to eliminate the pushing down of the
    divs underneath

--- views/templates/header.php
+++ views/templates/header.php
@@ -3,36 +3,45 @@
 	<link href="assets/css/style.css" type="text/css" rel="stylesheet"  />
 	
 </head>
-<body>	
-	<div class="col-1-1 navbar">		
-		<div class="col-1-8 topinfo">
-			<a href="?controller=matterentry&action=view<?php cmurl();?>">Matter Entry</a>
-		</div>
-		<div class="col-1-8 topinfo">
-			<a href="?controller=mp&action=view&clnt=<?php cmurl();?>">Matter Profile</a>
-		</div>		
-		<div class="col-1-8 topinfo">
-			<a href="?controller=address&action=view<?php cmurl();?>">Address Info</a>
-		</div>
-		<div class="col-1-8 topinfo">
-			<a href="?controller=parties&action=view<?php cmurl();?>">Parties/Conflicts</a>
-		</div>
-		<div class="col-1-8 topinfo submenu">
-			Admin
-			<div class="col-1-1 submenuitem">
-				<a href="#">EMD</a>
-			</div>
-			<div class="col-1-1 submenuitem">
-				<a href="#">Table Mtnc</a>
-			</div>
-		</div>	
-		<div class="col-1-8 topinfo">
-			<div style="color:blue;">a</div>
-		</div>			
-		<div class="col-1-4 topinfo">
-			<div style="color:blue;">a</div>
-		</div>	
-	</div>
-			
+
+<body>
+<nav>
+    <ul>
+        <li>
+            <a href="?controller=matterentry&action=view<?php cmurl();?>">Matter Entry</a>            
+        </li>
+        <li>
+            <a href="?controller=mp&action=view&clnt=<?php cmurl();?>">Matter Profile</a>
+        </li>
+        <li>
+            <a href="?controller=address&action=view<?php cmurl();?>">Address Info</a>
+        </li>
+        <li>
+            <a href="?controller=parties&action=view<?php cmurl();?>">Parties/Conflicts</a>
+        </li>
+        <li>
+            <a href="#">Admin</a>
+            <ul>
+                <li><a href="#">e-Matter Data</a></li>
+                <li><a href="#">Table Maintenance</a></li>
+                <li><a href="#">Bulk Recon Note Upload</a></li>
+                <li><a href="#">Client Services</a></li>
+            </ul>
+        </li>
+        <li>
+            <a href="#">Budgeting</a>
+            <ul>
+                <li><a href="#">Budget vs Actuals</a></li>
+                <li><a href="#">Budget Admin</a></li>
+                <li><a href="#">Budget Summary</a></li>
+            </ul>
+        </li>        
+    </ul>
+</nav>
+
+
+		
+	
+
 
 
